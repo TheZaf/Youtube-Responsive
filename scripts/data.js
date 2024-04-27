ba20f926781dced6e0a49e39bab50b0f7fb3272c
TheZaf <128605103+TheZaf@users.noreply.github.com>
2024-04-27 12:23:44 +0530

    Add files via upload

---
+++ scripts/data.js
@@ -0,0 +1,286 @@
+const videoInfo = [{
+     thumbnail:'source/ui_interface/thumbnails/image1.jpeg',
+     profile:'source/ui_interface/profiles/profile1.jpg',
+     title:'Tech Vs Youtubers🤯Tech Collab',
+     channel:'A2D Channel',
+     link:'https://youtu.be/sloNgBqjz6c?si=69WxY1BM0VsPL-6A',
+     videoStat:{
+        views:'18.2M views',
+        days:'2 weeks ago',
+        length: '25:30'
+     }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image2.jpeg',
+    profile:'source/ui_interface/profiles/profile2.jpg',
+    title:'Way Down We Go [BEST PART 1 HOUR LOOP]',
+    channel:' The Music Spot [T.M.S] ',
+    link:'https://youtu.be/nBHMmUekBh8?si=mfaxUTeH7gqYiBM0',
+    videoStat:{
+        views:'797k views',
+        days: "5 months ago",
+        length: ' 1:00:17 '
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image3.webp',
+    profile:'source/ui_interface/profiles/profile3.jpg',
+    title:'Avatar Frontiers of Pandora vs RDR 2',
+    channel:'GameBest',
+    link:'https://youtu.be/TneEgi9pur0?si=VY0o8lOJwjt-kBO3',
+    videoStat:{ 
+        views:'1M views',
+        days: "2 months ago",
+        length: '10:07'
+
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image4.webp',
+    profile:'source/ui_interface/profiles/profile4.jpg',
+    title:'Memory Reboot Slowed',
+    channel:'Narvent',
+    link:'https://youtu.be/wL8DVHuWI7Y?si=-7bb7A1EVyq10IAP',
+    videoStat:{ 
+        views:'62M views',
+        days: "1 year ago",
+        length: '3:57'
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image5.webp',
+    profile:'source/ui_interface/profiles/profile5.jpg',
+    title:`Red Dead Redemption 2 Collector's Box Unboxing`,
+    channel:'TheRelaxingEnd',
+    link:'https://youtu.be/HiEPNNVvqTY?si=cQgiB6Ho1fqGKI83',
+    videoStat:{ 
+        views:'7.5M views',
+        days: "5 year ago",
+        length: '23:36'
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image6.webp',
+    profile:'source/ui_interface/profiles/profile6.jpg',
+    title:`Introducing - 𝐇𝐎𝐌𝐈𝐄𝟓`,
+    channel:'HOMIE5',
+    link:'https://youtu.be/n-rL0rWpCa0?si=g5Wh13ChjoT5Thms',
+    videoStat:{ 
+        views:'650K views',
+        days: "7 days ago",
+        length: '2:23'
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image7.webp',
+    profile:'source/ui_interface/profiles/profile7.jpg',
+    title:`Arthur Morgan || Lovely`,
+    channel:'HOMIE5',
+    link:'https://youtu.be/p5yUKjOOWvM?si=CyNowQHv_gi4VSNt',
+    videoStat:{ 
+        views:'2.5M views',
+        days: "3 years ago",
+        length: '3:22'
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image8.webp',
+    profile:'source/ui_interface/profiles/profile8.jpg',
+    title:`I Bought My First Car... ...`,
+    channel:'IShowSpeed',
+    link:'https://youtu.be/5j8V3wHw1ls?si=1hPACiGewy641CWD',
+    videoStat:{ 
+        views:'3M views',
+        days: "2 weeks ago",
+        length: '7:09'
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image9.webp',
+    profile:'source/ui_interface/profiles/profile9.jpg',
+    title:`Sai Abhyankkar - Katchi Sera (Music Video) `,
+    channel:'Think Music India',
+    link:'https://youtu.be/VU23OPQ1Pmc?si=O3PjOUjnyUxKGyGI',
+    videoStat:{ 
+        views:'83M views',
+        days: "2 months ago",
+        length: '3:05'
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image10.webp',
+    profile:'source/ui_interface/profiles/profile10.jpg',
+    title:`Kodaikanal Series  Full Movie🔥4K`,
+    channel:'Vj Siddhu Vlogs',
+    link:'https://youtu.be/YrXAUN3p1IM?si=ZS-78mgfMHrQA8Rd',
+    videoStat:{ 
+        views:'2.8M views',
+        days: "2 months ago",
+        length: '2:53:15'
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image11.webp',
+    profile:'source/ui_interface/profiles/profile11.jpg',
+    title:`Never Have I Ever With Our F1 Drivers!`,
+    channel:'FORMULA 1',
+    link:'https://youtu.be/50bXYIdVOyU?si=Y8CnWikj1S8ivf_G',
+    videoStat:{ 
+        views:'1M views',
+        days: "2 day ago",
+        length: '9:36'
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image12.webp',
+    profile:'source/ui_interface/profiles/profile12.jpg',
+    title:`Randy Orton RKOs IShowSpeed`,
+    channel:'WWE',
+    link:'https://youtu.be/za4pLoqlpxE?si=4LklS8Ouzwbr4bFx',
+    videoStat:{ 
+        views:'3.4M views',
+        days: "6 weeks ago",
+        length: '1:09'
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image13.webp',
+    profile:'source/ui_interface/profiles/profile13.jpg',
+    title:'ROLLS ROYCE HELP ME REBUILD MARCUS RASHFORDS WRAITH',
+    channel:'Mat Armstrong',
+    link:'https://youtu.be/3Sf-GwTNGQA?si=E-dIhYyJ61I0vBeM',
+    videoStat:{ 
+        views:'3.2M views',
+        days: "2 days ago",
+        length: '36:00'
+
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image14.webp',
+    profile:'source/ui_interface/profiles/profile14.jpg',
+    title:'Grand Theft Auto VI Trailer 1',
+    channel:'Rockstar Games',
+    link:'https://youtu.be/QdBZY2fkU-0?si=AMESC0QO3CKS_zC6',
+    videoStat:{ 
+        views:'187M views',
+        days: "4 months ago",
+        length: '1:31'
+
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image15.webp',
+    profile:'source/ui_interface/profiles/profile6.jpg',
+    title:' LOSER EATS DOG FOOD 🤢 - HOMIE5 ',
+    channel:'HOMIE5',
+    link:'https://youtu.be/_piHkhbYM1E?si=y4RbbCeu3r5ZleKW0',
+    videoStat:{ 
+        views:'807K views',
+        days: "4 days ago",
+        length: '25:02'
+
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image16.webp',
+    profile:'source/ui_interface/profiles/profile16.jpg',
+    title:'Ages 1 - 100 Decide Who Wins $250,000',
+    channel:'MrBeast',
+    link:'https://youtu.be/l-nMKJ5J3Uc?si=26TClE7olTgDz3GQ',
+    videoStat:{ 
+        views:'88M views',
+        days: "4 days ago",
+        length: '40:02'
+
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image17.webp',
+    profile:'source/ui_interface/profiles/profile10.jpg',
+    title:'எனக்கே விபூதி அடிக்க பாத்தல🤔',
+    channel:'Vj Siddhu Vlogs',
+    link:'https://youtu.be/1zBuKn3KXFg?si=zMN1cdVk7HqXC-TM',
+    videoStat:{ 
+        views:'2.2M views',
+        days: "2 days ago",
+        length: '19:53'
+
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image18.webp',
+    profile:'source/ui_interface/profiles/profile11.jpg',
+    title:'Race Highlights | 2024 Chinese Grand Prix',
+    channel:'FORMULA 1',
+    link:'https://youtu.be/hh4prBn66Dc?si=8ClC8s-COfjNeSRR',
+    videoStat:{ 
+        views:'5.3M views',
+        days: "3 days ago",
+        length: '8:10'
+
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image19.webp',
+    profile:'source/ui_interface/profiles/profile18.jpg',
+    title:' If I Laugh, I Go Bald❗👩‍🦲#TNTL3',
+    channel:'Yeah Tube',
+    link:'https://youtu.be/CdFULEthk2g?si=QoHDb_43UjDTSBUo',
+    videoStat:{ 
+        views:'351K views',
+        days: "1 years ago",
+        length: '14:43'
+
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image20.webp',
+    profile:'source/ui_interface/profiles/profile1.jpg',
+    title:'AI Uruttu Everywhere!😡 போதும்டா சாமி 🙏',
+    channel:'A2D Channel',
+    link:'https://youtu.be/yMYCkcB-5Gs?si=dPUuZZXMPiQjDFkp',
+    videoStat:{ 
+        views:'343K views',
+        days: "19 hours ago",
+        length: '11:43'
+
+    }
+},{
+    thumbnail:'source/ui_interface/thumbnails/image21.webp',
+    profile:'source/ui_interface/profiles/profile19.jpg',
+    title:'Workout Challenge Gone Wrong!',
+    channel:'Tharun Kumar',
+    link:'https://youtu.be/f6FpyJk7pg8?si=qKARHjmrzUduJpSK',
+    videoStat:{ 
+        views:'341K views',
+        days: "13 days ago",
+        length: '12:43'
+
+    }
+}
+];
+
+let videoHTML = '';
+
+videoInfo.forEach((video) => {
+    videoHTML +=
+    ` <div class="video-container">
+    <div class="video-thumbnail">
+         <a href="${video.link}">
+            <img class="thumbnail" 
+            src="${video.thumbnail}">
+         </a>
+            <div class="video-timer">${video.videoStat.length}</div>
+        </div>
+    <div class=" video-info">
+        <div class="channel-profile"> 
+        <a href="https://www.youtube.com/channel/UCn4rEMqKtwBQ6-oEwbd4PcA"><img class="profile"
+            src="${video.profile}">
+        </a>    
+        </div>
+        <div class="video-states">
+            <p style="margin-bottom:5px;"
+            ><strong>${video.title}</strong></p>
+            <div class ="stat">
+            <p class="p1" style="color: #606060;
+
+            margin-top:0px;
+            margin-bottom:0px;
+            margin-right:5px;
+            ">${video.channel}</p>
+            <p class="p2"  style="color: #606060;
+
+            margin-top:0px;
+            margin-bottom:0px;">${video.videoStat.views}&nbsp;•&nbsp;${video.videoStat.days} </p>
+            </div>
+        </div>
+
+    </div>
+</div>`
+
+});
+
+
+document.querySelector('.js-video-grid').innerHTML = videoHTML;
